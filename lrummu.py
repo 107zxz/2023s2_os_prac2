fd9d65355951b214b1395fa7cf281beaf6843f93
107zxz <i107zxz@icloud.com>
2023-09-02 19:09:37 +0930

    Major code cleanup

--- lrummu.py
+++ lrummu.py
@@ -4,14 +4,13 @@ class LruMMU(MMU):
 
     debug: bool
 
+    cache: set[int]
     cache_size: int
 
     timestep: int
 
     last_uses: dict[int, int]
     dirty_pages: set[int]
-
-    frames: set[int]
 
     disk_reads: int
     disk_writes: int
@@ -20,7 +19,7 @@ class LruMMU(MMU):
     def __init__(self, frames: int):
         self.cache_size = frames
         self.timestep = 0
-        self.frames = set()
+        self.cache = set()
         self.last_uses = {}
         self.dirty_pages = set()
         self.disk_reads = 0
@@ -33,90 +32,53 @@ class LruMMU(MMU):
     def reset_debug(self):
         self.debug = False
 
-    def read_memory(self, page_number: int):
+    def cache_page(self, page_number: int):
 
-        # print("Counted disk read!")
-
-        # Track a page fault if relevant
-        if page_number not in self.frames:
+        # Detect page fault
+        if page_number not in self.cache:
+            
+            # Page faults require a disk read
             self.page_faults += 1
             self.disk_reads += 1
             
             # Is cache full?
-            if len(self.frames) == self.cache_size:
+            if len(self.cache) == self.cache_size:
+                
                 # Kick out the oldest
                 oldest = self.timestep
                 oldest_frame = None
 
-                for frame in self.frames:
+                for frame in self.cache:
                     if self.last_uses[frame] < oldest:
                         oldest = self.last_uses[frame]
                         oldest_frame = frame
                 
-                self.frames.remove(oldest_frame)
+                self.cache.remove(oldest_frame)
 
+                # If we're evicting a dirty page, we're writing it to disk
                 if oldest_frame in self.dirty_pages:
                     self.disk_writes += 1
                     self.dirty_pages.remove(oldest_frame)
 
 
         # Add page to frames
-        self.frames.add(page_number)
+        self.cache.add(page_number)
 
         # Track this use
         self.last_uses[page_number] = self.timestep
 
-        # Track disk read
-        # self.disk_reads += 1
-
         # Step forward in time
         self.timestep += 1
+        
 
-        # print("Reading page {}, {} total".format(page_number, self.disk_reads))
-        # print(self.frames)
+    def read_memory(self, page_number: int):
+        self.cache_page(page_number)
 
     def write_memory(self, page_number: int):
 
-
-        # Track a page fault if relevant
-        if page_number not in self.frames:
-            self.page_faults += 1
-            self.disk_reads += 1
-
-            # Is cache full?
-            if len(self.frames) == self.cache_size:
-                # Kick out the oldest
-                oldest = self.timestep
-                oldest_frame = None
-
-                for frame in self.frames:
-                    if self.last_uses[frame] < oldest:
-                        oldest = self.last_uses[frame]
-                        oldest_frame = frame
-                
-                self.frames.remove(oldest_frame)
-
-                if oldest_frame in self.dirty_pages:
-                    self.disk_writes += 1
-                    self.dirty_pages.remove(oldest_frame)
-
-        # Add page to frames
-        self.frames.add(page_number)
-
-        # Track this use
-        self.last_uses[page_number] = self.timestep
-
-        # Mark dirty
         self.dirty_pages.add(page_number)
 
-        # Track disk write
-        # self.disk_writes += 1
-
-        # Step forward in time
-        self.timestep += 1
-
-        # print("Writing page {}".format(page_number))
-        # print(self.frames)
+        self.cache_page(page_number)
 
     def get_total_disk_reads(self):
         return self.disk_reads
