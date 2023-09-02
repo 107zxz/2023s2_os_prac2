5c0929fae02f502fb077a08ca231e0765cfefa9d
107zxz <i107zxz@icloud.com>
2023-09-02 19:14:45 +0930

    added debug

--- lrummu.py
+++ lrummu.py
@@ -34,12 +34,18 @@ class LruMMU(MMU):
 
     def cache_page(self, page_number: int):
 
+        if self.debug:
+            print("Caching page {}".format(page_number))
+
         # Detect page fault
         if page_number not in self.cache:
             
             # Page faults require a disk read
             self.page_faults += 1
             self.disk_reads += 1
+
+            if self.debug:
+                print("Page Fault Disk reads: {}".format(self.disk_reads))
             
             # Is cache full?
             if len(self.cache) == self.cache_size:
@@ -53,12 +59,20 @@ class LruMMU(MMU):
                         oldest = self.last_uses[frame]
                         oldest_frame = frame
                 
+                if self.debug:
+                    print("Evicting page {}".format(oldest_frame))
+
                 self.cache.remove(oldest_frame)
 
                 # If we're evicting a dirty page, we're writing it to disk
                 if oldest_frame in self.dirty_pages:
                     self.disk_writes += 1
                     self.dirty_pages.remove(oldest_frame)
+
+                    if self.debug:
+                        print("Page dirty, disk writes: {}".format(self.disk_writes))
+            
+            
 
 
         # Add page to frames
@@ -69,14 +83,29 @@ class LruMMU(MMU):
 
         # Step forward in time
         self.timestep += 1
+
+        if self.debug:
+            print("Current cache:")
+            print(self.cache)
         
 
     def read_memory(self, page_number: int):
+
+        if self.debug:
+            print("Reading page {}".format(page_number))
+
         self.cache_page(page_number)
 
     def write_memory(self, page_number: int):
 
+        if self.debug:
+            print("Writing page {}".format(page_number))
+
         self.dirty_pages.add(page_number)
+
+        if self.debug:
+            print("Dirty pages:")
+            print(self.dirty_pages)
 
         self.cache_page(page_number)
 
